9f8782add1500e7e23144a745257434c8d683e2a
Jonathan Co <jonathanco1991@gmail.com>
2015-06-16 15:14:52 +0100

    Added basic constraint trace graph components

---
+++ incremental-validation/src/org/eclipse/epsilon/evl/trace/TraceElement.java
@@ -0,0 +1,14 @@
+package org.eclipse.epsilon.evl.trace;
+
+import com.tinkerpop.blueprints.Element;
+
+/**
+ * The {@link TraceElement} interface represents an element in the constraint
+ * trace graph (generally a node or edge).
+ * 
+ * @author Jonathan Co
+ *
+ */
+public interface TraceElement extends Element {
+
+}
