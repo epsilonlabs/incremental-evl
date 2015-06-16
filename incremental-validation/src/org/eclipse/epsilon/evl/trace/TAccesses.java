802475344d7090743f8590aaaff1236dcdcfea1d
Jonathan Co <jonathanco1991@gmail.com>
2015-06-16 16:12:13 +0100

    Added all basic elements of the trace graph

---
+++ incremental-validation/src/org/eclipse/epsilon/evl/trace/TAccesses.java
@@ -0,0 +1,33 @@
+package org.eclipse.epsilon.evl.trace;
+
+import com.tinkerpop.frames.InVertex;
+import com.tinkerpop.frames.OutVertex;
+
+/**
+ * The {@link TAccesses} interface represents an edge between a scope and a
+ * single property accessed by that scope in the trace graph.
+ * 
+ * @author Jonathan Co
+ *
+ */
+public interface TAccesses extends TraceEdge {
+
+	/**
+	 * Common name of this trace element
+	 */
+	String TRACE_TYPE = "accesses";
+
+	/**
+	 * @return The scope that is accessing the property at
+	 *         {@link #getProperty()}.
+	 */
+	@OutVertex
+	TScope getScope();
+
+	/**
+	 * @return The property that has been accessed at {@link #getScope()}.
+	 */
+	@InVertex
+	TProperty getProperty();
+	
+}
---
+++ incremental-validation/src/org/eclipse/epsilon/evl/trace/TProperty.java
@@ -0,0 +1,85 @@
+package org.eclipse.epsilon.evl.trace;
+
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.frames.Adjacency;
+import com.tinkerpop.frames.Property;
+
+/**
+ * The {@link TProperty} interface represents a model element property vertex
+ * in the trace graph.
+ * 
+ * @author Jonathan Co
+ *
+ */
+public interface TProperty extends TraceVertex {
+
+	/**
+	 * Common name of this trace element
+	 */
+	String TRACE_TYPE = "property";
+
+	/**
+	 * Key to the 'name' property of this property
+	 */
+	String NAME = "name";
+
+	/**
+	 * @return The name of this property
+	 */
+	@Property(TProperty.NAME)
+	String getName();
+
+	/**
+	 * Set the name of this property
+	 * 
+	 * @param name
+	 *            The new name of this property
+	 */
+	@Property(TProperty.NAME)
+	void setName(String name);
+
+	/**
+	 * Get the model element that owns this property.
+	 * 
+	 * @return The owning model element.
+	 */
+	@Adjacency(label = TOwns.TRACE_TYPE, direction = Direction.IN)
+	TElement getOwner();
+
+	/**
+	 * Set the model element that owns this property.
+	 * 
+	 * @param element
+	 *            The new model element that owns this property.
+	 */
+	@Adjacency(label = TOwns.TRACE_TYPE, direction = Direction.IN)
+	TElement setOwner(TElement element);
+
+	/**
+	 * Get the scopes that access this property
+	 * 
+	 * @return An {@link Iterable} containing all the relevant scopes.
+	 */
+	@Adjacency(label = TAccesses.TRACE_TYPE, direction = Direction.IN)
+	Iterable<TScope> getScopes();
+
+	/**
+	 * Add a scope that accesses this property.
+	 * 
+	 * @param scope
+	 *            The scope to add.
+	 * @return the original scope parameter but linked to this property.
+	 */
+	@Adjacency(label = TAccesses.TRACE_TYPE, direction = Direction.IN)
+	TScope addScope(TScope scope);
+
+	/**
+	 * Remove a scope that accesses this property.
+	 * 
+	 * @param scope
+	 *            The scope to remove.
+	 */
+	@Adjacency(label = TAccesses.TRACE_TYPE, direction = Direction.IN)
+	void removeScope(TScope scope);
+
+}
