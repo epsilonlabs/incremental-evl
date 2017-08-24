fa75840c9c4fed1e536f3790a8c649ff8f2498a8
Unknown <arcanefoam@gmail.com>
2017-08-24 18:13:58 +0100

    Finish the templates for the OrientDB code generation

--- org.eclipse.epsilon.evl.incremental.orientdb/src/org/eclipse/epsilon/evl/incremental/orientdb/dialog/OrientDBManagerConfiguration.java
+++ org.eclipse.epsilon.evl.incremental.orientdb/src/org/eclipse/epsilon/evl/incremental/orientdb/dialog/OrientDBManagerConfiguration.java
@@ -1,6 +1,7 @@
 package org.eclipse.epsilon.evl.incremental.orientdb.dialog;
 
 import org.eclipse.epsilon.common.incremental.dt.launching.dialogs.AbstractTraceManagerConfigurationDialog;
+import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
@@ -9,23 +10,25 @@ import org.eclipse.swt.widgets.Text;
 
 public class OrientDBManagerConfiguration extends AbstractTraceManagerConfigurationDialog {
 
-	private Text databaseUriText;
+	public static final String DB_URL = "url";
+
+	public static final String DB_USER = "user";
+
+	public static final String DB_PASS = "pass";
+
+	public static final String DB_CREATE = "create";
+	
+	private Text databaseUrlText;
 	private Text userText;
 	private Text passwordText;
 
 	@Override
-	protected void loadProperties() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	protected void createGroups(Composite parent) {
-		final Composite groupContent = createGroupContainer(parent, "Connection information", 3);
+		final Composite groupContent = createGroupContainer(parent, "Connection information", 2);
 		Label databaseUriLabel = new Label(groupContent, SWT.NONE);
 		databaseUriLabel.setText("DataBase URI: ");
-		databaseUriText = new Text(groupContent, SWT.BORDER);
-		databaseUriText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		databaseUrlText = new Text(groupContent, SWT.BORDER);
+		databaseUrlText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		
 		Label userLabel = new Label(groupContent, SWT.NONE);
@@ -42,14 +45,26 @@ public class OrientDBManagerConfiguration extends AbstractTraceManagerConfigurat
 
 	@Override
 	protected String getExecutionTraceManagerName() {
-		// TODO Auto-generated method stub
 		return "OrientDB Trace Manager";
+	}
+	
+	@Override
+	protected void loadProperties() {
+		if (properties == null) return;
+		databaseUrlText.setText(properties.getProperty(DB_URL));
+		// FIXME add a save credentials button (safety risk)
+		userText.setText(properties.getProperty(DB_USER));
+		passwordText.setText(properties.getProperty(DB_PASS));
 	}
 
 	@Override
-	public String[] getConfigurationParameters() {
-		String[] config = {databaseUriText.getText(), userText.getText(), passwordText.getText()};
-		return config;
+	protected void storeProperties() {
+		properties = new StringProperties();
+		properties.put(DB_URL, databaseUrlText.getText());
+		properties.put(DB_USER, userText.getText());
+		properties.put(DB_PASS, passwordText.getText());
+		// FIXME Add check button for this
+		properties.put(DB_CREATE, true);
 	}
 
 }
