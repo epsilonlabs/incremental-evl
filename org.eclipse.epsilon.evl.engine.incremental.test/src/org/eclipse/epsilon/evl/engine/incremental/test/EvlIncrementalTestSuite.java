ab825993d81da2b244aa8daea0e8c896ab0063f4
Horacio Hoyos Rodriguez <arcanefoam@gmail.com>
2017-11-10 17:49:32 +0000

    Add lifecycle tests for EVL trace model

---
+++ org.eclipse.epsilon.evl.engine.incremental.test/src/org/eclipse/epsilon/evl/engine/incremental/test/EvlIncrementalTestSuite.java
@@ -0,0 +1,22 @@
+package org.eclipse.epsilon.evl.engine.incremental.test;
+
+
+import org.eclipse.epsilon.evl.engine.incremental.trace.EvlExecutionTraceLifecycleTest;
+import org.eclipse.epsilon.evl.incremental.trace.EvlTraceModelTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+
+@RunWith(Suite.class)
+@SuiteClasses({EvlTraceModelTests.class,
+			   EvlExecutionTraceLifecycleTest.class})
+public class EvlIncrementalTestSuite {
+	
+	public static Test suite() {
+		return new JUnit4TestAdapter(EvlIncrementalTestSuite.class);
+	}
+
+}
